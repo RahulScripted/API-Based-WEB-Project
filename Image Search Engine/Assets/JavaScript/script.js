1389d5895fd4708bfb15434d6834bd004648104a
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-10-24 14:53:18 +0530

    BAsic Structure Created

---
+++ Image Search Engine/Assets/JavaScript/script.js
@@ -0,0 +1,16 @@
+const token = ""
+
+let searchText = document.getElementById("txt");
+let searchBtn = document.getElementById("btn");
+let imgCard = document.getElementById("imgCards");
+let show = document.getElementById("show");
+
+let page = 1;
+let keyWord = "";
+
+
+const getImage = async () => {
+    
+}
+
+searchBtn.addEventListener("click",getImage());
